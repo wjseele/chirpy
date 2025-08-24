4a90faf123ca5fee79d56d4103dfae7ea23d2920
wjseele <wjseele@pm.me>
2025-08-24 12:36:32 +0200

    More handlers

--- main.go
+++ main.go
@@ -5,7 +5,12 @@ import (
 	"io"
 	"net/http"
 	"os"
+	"sync/atomic"
 )
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
 
 func main() {
 	serveMux := http.NewServeMux()
@@ -13,7 +18,9 @@ func main() {
 		Addr:    ":8080",
 		Handler: serveMux,
 	}
-	serveMux.Handle("/app/", http.StripPrefix("/app/", http.FileServer(http.Dir("."))))
+	apiCfg := apiConfig{}
+
+	serveMux.Handle("/app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app/", http.FileServer(http.Dir(".")))))
 	serveMux.HandleFunc("/healthz", func(w http.ResponseWriter, req *http.Request) {
 		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 		w.WriteHeader(http.StatusOK)
@@ -23,7 +30,37 @@ func main() {
 			os.Exit(1)
 		}
 	})
+
+	serveMux.HandleFunc("/metrics", apiCfg.handlerCounter)
+	serveMux.HandleFunc("/reset", apiCfg.handlerReset)
+
 	err := server.ListenAndServe()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	cfg.fileserverHits.Add(1)
+	return next
+}
+
+func (cfg *apiConfig) handlerCounter(w http.ResponseWriter, req *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	_, err := io.WriteString(w, fmt.Sprintf("%v", cfg.fileserverHits.Load()))
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
+func (cfg *apiConfig) handlerReset(w http.ResponseWriter, req *http.Request) {
+	cfg.fileserverHits.Store(0)
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	_, err := io.WriteString(w, fmt.Sprintf("Reset counter to %v", cfg.fileserverHits.Load()))
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
