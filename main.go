662dae80f4e672480228f5742e7e3a79c74977b8
wjseele <wjseele@pm.me>
2025-08-24 11:22:49 +0200

    Basics are working

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
+
+func main() {
+	serveMux := http.NewServeMux()
+	server := http.Server{
+		Addr:    ":8080",
+		Handler: serveMux,
+	}
+	err := server.ListenAndServe()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
