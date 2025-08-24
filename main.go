acc0d4bd675f81bbfa3314f6b3289746de310a91
wjseele <wjseele@pm.me>
2025-08-24 11:55:36 +0200

    Serving even more

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
 	"os"
 )
@@ -12,7 +13,16 @@ func main() {
 		Addr:    ":8080",
 		Handler: serveMux,
 	}
-	serveMux.Handle("/", http.FileServer(http.Dir(".")))
+	serveMux.Handle("/app/", http.StripPrefix("/app/", http.FileServer(http.Dir("."))))
+	serveMux.HandleFunc("/healthz", func(w http.ResponseWriter, req *http.Request) {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		_, err := io.WriteString(w, "OK")
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+	})
 	err := server.ListenAndServe()
 	if err != nil {
 		fmt.Println(err)
